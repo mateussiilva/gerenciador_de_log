0e257df14bcb44c050a7042e1e0241524c5d58f1
mateussiilva <mateuspython3010@gmail.com>
2023-07-02 22:34:41 -0300

    Logica melhjorada em 10000%

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-
-
+import re
 
 
 # abrir arquivo e ler linhas 
@@ -15,26 +14,48 @@ FIM_TABELA = '</TABLE>'
 
 def get_linhas_for_array(name_file):
     list_lines = []
-    arquivo = open(f"{name_file}","r+",encoding="utf-8")
+    arquivo = open(f"{name_file}","r+")
     for indice,linhas in enumerate(arquivo.readlines()):
         if len(linhas) > 2:
             list_lines.append(linhas)
-    arquivo.close()
-    
+    arquivo.close()    
     return list_lines
 
 
-if __name__ == "__main__":
-    linhas_arquivos = get_linhas_for_array("RIPLOG.txt")
-    for indice,linhas in enumerate(linhas_arquivos):
-        linha_limpa =  linhas.strip("\n")
-        if linha_limpa ==  INCIO_TABELA:
-            inicio = indice
-        if linha_limpa == FIM_TABELA:
-            fim = indice
-            fim += 1
-            
-    for linha in linhas_arquivos[inicio:fim]:
-        print(linha)
+def remove_html(text):
+    p = re.compile(r'<.*?>')
+    return re.sub(p,"",text)
 
+
+def separete_tables(vetor):
+    lista_tabelas = []
+    temp = []
+    for indice,linha in enumerate(vetor):
+        texto = linha.rstrip("\n\n")
+        texto_sem_html = remove_html(texto)
+        if len(texto_sem_html) > 2:
+            temp.append(texto_sem_html)
+        if  texto == FIM_TABELA:
+            lista_tabelas.append(temp[:])
+            temp.clear()
+    return lista_tabelas
+
+
+def gravar_arquivo_log_principal(matrix,name_file):
+    arquivo = open(f"{name_file}","w+")
+    for vetores in matrix:
+        for linha in vetores:
+            texto_linha = linha + "\n"
+            arquivo.write(texto_linha)
+        arquivo.write("-------------------------\n")
+    arquivo.close()
+
+
+if __name__ == "__main__":
+    linhas_arquivo = get_linhas_for_array("RIPLOG.txt")
+    lista_tabelas_separadas = separete_tables(linhas_arquivo)
+    gravar_arquivo_log_principal(lista_tabelas_separadas,name_file="log_meio_limpo.log")
+
+
+    
 
